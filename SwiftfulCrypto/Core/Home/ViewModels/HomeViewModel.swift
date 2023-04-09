eaa628dcd0079e9b8d61537c234ab9d524a3a0a5
gmekv <113336356+gmekv@users.noreply.github.com>
2023-04-09 15:26:31 +0400

    commit 1

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -17,16 +17,31 @@ class HomeViewModel: ObservableObject {
     private var cancellables = Set<AnyCancellable>()
     
     init(){
-addSubscribers()
+        addSubscribers()
         
     }
     
     
     func addSubscribers() {
-        dataService.$allCoins
+        //updates all coins
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self]  (returnedCoins) in
                 self?.allCoins = returnedCoins
+                
             }
             .store(in: &cancellables)
     }
-}
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        let lowerCasedText = text.lowercased()
+        return coins.filter {(coin) -> Bool in return coin.name.lowercased().contains(lowerCasedText) ||
+            coin.symbol.lowercased().contains(lowerCasedText) || coin.id.lowercased().contains(lowerCasedText)
+        }
+        
+    }}
---
+++ SwiftfulCrypto/Core/Home/Views/StatisticView.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticView.swift
+//  SwiftfulCrypto
+//
+//  Created by Giorgi Meqvabishvili on 09.04.23.
+//
+
+import Foundation
