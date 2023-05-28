0b97082799cf7251c1bb10c1d38d422236d571bf
gmekv <113336356+gmekv@users.noreply.github.com>
2023-05-28 15:43:30 +0400

    commit3

--- SwiftfulCrypto/SwiftfulCryptoApp.swift
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -5,27 +5,40 @@
 //  Created by Giorgi Meqvabishvili on 04.04.23.
 //
 
+
 import SwiftUI
 
 @main
 struct SwiftfulCryptoApp: App {
     
     @StateObject private var vm = HomeViewModel()
-    
+    @State private var showLaunchView: Bool = true
     
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
-        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
-
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().tintColor = UIColor(Color.theme.accent)
+        UITableView.appearance().backgroundColor = UIColor.clear
     }
     
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                HomeView()
-                    .navigationBarHidden(true)
+            ZStack {
+                NavigationView {
+                    HomeView()
+                        .navigationBarHidden(true)
+                }
+                .navigationViewStyle(StackNavigationViewStyle())
+                .environmentObject(vm)
+
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .zIndex(2.0)
             }
-            .environmentObject(vm)
         }
     }
 }
